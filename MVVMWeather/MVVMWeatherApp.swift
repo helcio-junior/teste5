dd1ef44fff0ce39ca7cc854ac17cf569e39f3cc8
helcio-junior <helcio.0022@gmail.com>
2021-05-19 15:11:32 -0300

    Initial Commit

---
+++ MVVMWeather/MVVMWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMWeatherApp.swift
+//  MVVMWeather
+//
+//  Created by Helcio Junior on 19/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MVVMWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
